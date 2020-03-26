f08f3f1bad748c3d34c037a05eaece7958a8ac82
Alex Pariseau <arpariseau@gmail.com>
2020-03-26 14:03:33 -0600

    Built framework for validating consecutive placement

--- lib/board.rb
+++ lib/board.rb
@@ -6,7 +6,10 @@ class Board
   attr_reader :cells
 
   def initialize
-    coord = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    coord = ["A1", "A2", "A3", "A4",
+             "B1", "B2", "B3", "B4",
+             "C1", "C2", "C3", "C4",
+             "D1", "D2", "D3", "D4"]
     @cells = {}
       coord.each do |coord|
         @cells[coord] = Cell.new(coord)
@@ -19,20 +22,36 @@ class Board
 
   def valid_placement?(ship, coords)
     if ship.length != coords.length
-      return false
+      false
+    elsif !coords.map {|coord| valid_coordinate?(coord)}.all?
+      false
+#    elsif !cons_placement?(coords)
+#      false
     else
       true
     end
   end
 
-
-
-
-
-
-
-
-
-
+  def cons_placement?(coords)
+    rows = []
+    cols = []
+    coords.each do |coord|
+      row = coord[0]
+      rows << row
+      col = coord[1].to_i
+      cols << col
+    end
+#    binding.pry
+    if rows.uniq.length > 1 && cols.uniq.length > 1
+     false
+    elsif rows.uniq.length == 1
+  #   binding.pry
+  #   horizontal_cons(cols)
+     false
+    else
+  #   vertical_cons(rows)
+     false
+    end
+  end
 
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -5,35 +5,47 @@ require './lib/cell.rb'
 require './lib/board.rb'
 
 class BoardTest < Minitest::Test
-  def test_it_exists
-    board = Board.new
 
-    assert_instance_of Board, board
+  def setup
+    @board = Board.new
+  end
+
+  def test_it_exists
+    assert_instance_of Board, @board
   end
 
   def test_has_cells
-    board = Board.new
-
-    assert_equal 16, board.cells.keys.length
-    assert_equal 16, board.cells.values.length
-    assert_instance_of Cell, board.cells.values[0]
-    assert_instance_of Cell, board.cells.values[15]
+    assert_equal 16, @board.cells.keys.length
+    assert_equal 16, @board.cells.values.length
+    assert_instance_of Cell, @board.cells.values[rand(0..15)]
   end
 
   def test_can_validate_coordinates
-    board = Board.new
-
-    assert_equal true, board.valid_coordinate?("A1")
-    assert_equal false, board.valid_coordinate?("A5")
+    assert_equal true, @board.valid_coordinate?("A1")
+    assert_equal false, @board.valid_coordinate?("A5")
   end
 
   def test_can_validate_placement_length
-    board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
-    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
-    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(cruiser, ["A1", "A2"])
+    assert_equal true, @board.valid_placement?(submarine, ["A1", "A2"])
+  end
+
+  def test_can_validate_coordinate_placement
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal false, @board.valid_placement?(cruiser, ["A3", "A4", "A5"])
+    assert_equal true, @board.valid_placement?(submarine, ["A3", "A4"])
+  end
+
+  def test_can_validate_consecutive_placement
+    assert_equal false, @board.cons_placement?(["A1", "B2"])
+    assert_equal false, @board.cons_placement?(["A1", "A3", "A4"])
+    assert_equal false, @board.cons_placement?(["A1", "C1", "D1"])
+    assert_equal true, @board.cons_placement?(["A3", "A4"])
   end
 
 end
